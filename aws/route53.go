2abe319dcf350c672366834339858d6adeba5c7d
Lunjie Zhang <lunjie.zhang-extern@deutschebahn.com>
2020-02-24 16:35:00 +0100

    feat: Add ingress counter to avoid undesired deletion, Use networking api instead of extentions for ingress, Add CNAME support, Recordset type configurable with parameter (by external expert on behalf of DB Systel GmbH)

--- aws/route53.go
+++ aws/route53.go
@@ -18,29 +18,51 @@ type HostedZone struct {
 	id   string
 }
 
-// modify Amazon Route53 recordset with given state (upsert/delete)
-func ChangeRecordSet(state, aliasName, aliasHostedZoneId, name, hostedZoneId string) (string, error) {
+//
+func constructResourceRecordSet(aliasName, aliasHostedZoneID, name string, dnsType string) (resourceRecordSet *route53.ResourceRecordSet) {
+	if strings.ToUpper(dnsType) == "ALIAS" {
+		resourceRecordSet = &route53.ResourceRecordSet{
+			AliasTarget: &route53.AliasTarget{
+				DNSName:              aws.String(aliasName),
+				EvaluateTargetHealth: aws.Bool(true),
+				HostedZoneId:         aws.String(aliasHostedZoneID),
+			},
+			Name: aws.String(name),
+			Type: aws.String("A"),
+		}
+	} else {
+		resourceRecordSet = &route53.ResourceRecordSet{
+			ResourceRecords: []*route53.ResourceRecord{
+				{
+					Value: aws.String(aliasName),
+				},
+			},
+			TTL:  aws.Int64(300),
+			Name: aws.String(name),
+			Type: aws.String("CNAME"),
+		}
+	}
+
+	return resourceRecordSet
+}
+
+// ChangeRecordSet modifies Amazon Route53 recordset with given state (upsert/delete)
+func ChangeRecordSet(state, aliasName, aliasHostedZoneID, name, hostedZoneID string, dnsType string) (string, error) {
 	sess := session.Must(session.NewSession())
 	svc := route53.New(sess)
+
+	resourceRecordSet := constructResourceRecordSet(aliasName, aliasHostedZoneID, name, dnsType)
 
 	input := &route53.ChangeResourceRecordSetsInput{
 		ChangeBatch: &route53.ChangeBatch{
 			Changes: []*route53.Change{
 				{
-					Action: aws.String(state),
-					ResourceRecordSet: &route53.ResourceRecordSet{
-						AliasTarget: &route53.AliasTarget{
-							DNSName:              aws.String(aliasName),
-							EvaluateTargetHealth: aws.Bool(true),
-							HostedZoneId:         aws.String(aliasHostedZoneId),
-						},
-						Name: aws.String(name),
-						Type: aws.String("A"),
-					},
+					Action:            aws.String(state),
+					ResourceRecordSet: resourceRecordSet,
 				},
 			},
 		},
-		HostedZoneId: aws.String(hostedZoneId),
+		HostedZoneId: aws.String(hostedZoneID),
 	}
 
 	result, err := svc.ChangeResourceRecordSets(input)
